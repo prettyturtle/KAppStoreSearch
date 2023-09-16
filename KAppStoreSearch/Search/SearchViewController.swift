4df80ed04f12aec5051d021e730b67355af3074c
prettyturtle <unifconti@gmail.com>
2023-09-16 19:14:44 +0900

    Comment 추가

--- KAppStoreSearch/Search/SearchViewController.swift
+++ KAppStoreSearch/Search/SearchViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// MARK: - 검색 VC
 final class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
@@ -18,15 +19,20 @@ final class SearchViewController: UIViewController {
     
 }
 
+// MARK: - UISearchResultsUpdating
 extension SearchViewController: UISearchResultsUpdating {
+    // 검색 바의 텍스트가 변경될 때마다 호출
     func updateSearchResults(for searchController: UISearchController) {
+        
         let currentSearchText = searchController.searchBar.text ?? "" // 검색 텍스트
         
         print(currentSearchText)
     }
 }
 
+// MARK: - SET UP
 extension SearchViewController {
+    /// 검색 바 세팅
     func setupSearchBar() {
         let searchController = UISearchController(searchResultsController: SearchResultTableViewController())
         searchController.searchBar.placeholder = "게임, 앱, 스토리 등"
@@ -34,6 +40,7 @@ extension SearchViewController {
         navigationItem.searchController = searchController
     }
     
+    /// 내비게이션 바 세팅
     func setupNavigationBar() {
         navigationItem.title = "검색"
         navigationController?.navigationBar.prefersLargeTitles = true
