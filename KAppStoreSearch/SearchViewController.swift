4a24365b4adc6af808d4d39f9ec6b32060a756c8
prettyturtle <unifconti@gmail.com>
2023-09-16 16:49:35 +0900

    내비게이션 바, 검색 바 추가

--- KAppStoreSearch/SearchViewController.swift
+++ KAppStoreSearch/SearchViewController.swift
@@ -7,8 +7,26 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+final class SearchViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationBar()
+        setupSearchBar()
+    }
+    
+}
+
+extension SearchViewController {
+    func setupSearchBar() {
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "게임, 앱, 스토리 등"
+        navigationItem.searchController = searchController
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "검색"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
