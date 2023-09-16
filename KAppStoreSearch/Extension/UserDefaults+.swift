8815ea2ac394ea0b3fed9d5960d3972488a820b0
prettyturtle <unifconti@gmail.com>
2023-09-17 03:38:49 +0900

    UserDefaults+ 주석 추가 / UserDefaults+ remove, removeAll 구현 / UserDefaults Unit Test

--- KAppStoreSearch/Extension/UserDefaults+.swift
+++ KAppStoreSearch/Extension/UserDefaults+.swift
@@ -8,11 +8,17 @@
 import Foundation
 
 extension UserDefaults {
+    /// UserDefaults Key
     enum Key: String {
+        /// 최근 검색어 리스트
         case recentSearchTextList = "RECENT_SEARCH_TEXT_LIST"
+        /// 테스트용
         case test = "TEST_USER_DEFAULTS_KEY"
     }
     
+    /// 아이템 가져오기
+    ///
+    /// 해당 타입의 배열(옵셔널)을 반환한다.
     func fetch<T: Codable & Equatable>(key: Key) -> [T]? {
         guard let data = data(forKey: key.rawValue) else {
             return nil
@@ -25,6 +31,11 @@ extension UserDefaults {
         return decodedData
     }
     
+    /// 아이템 저장하기
+    ///
+    /// 배열의 0번째에 아이템을 저장한다.
+    ///
+    /// 만약 기존에 존재하는 아이템을 저장한다면, 배열의 0번째로 이동한다. (중복 저장 X)
     func save<T: Codable & Equatable>(_ item: T, key: Key) throws {
         var oldItems: [T] = fetch(key: key) ?? []
         
@@ -32,9 +43,32 @@ extension UserDefaults {
         
         let newItems = [item] + oldItems
         
+        try saveData(newItems, key: key)
+    }
+    
+    /// 아이템 삭제
+    ///
+    /// 한개의 아이템을 삭제한다
+    func remove<T: Codable & Equatable>(_ item: T, key: Key) throws {
+        var oldItems: [T] = fetch(key: key) ?? []
+        
+        oldItems = oldItems.filter { $0 != item }
+        
+        try saveData(oldItems, key: key)
+    }
+    
+    /// 모든 아이템 삭제
+    func removeAll(key: Key) {
+        setValue(nil, forKey: key.rawValue)
+    }
+    
+    /// 데이터 저장
+    ///
+    /// JSON Encoding 후 데이터 저장
+    private func saveData<T: Codable & Equatable>(_ items: [T], key: Key) throws {
         let encoder = JSONEncoder()
         
-        let encodedData = try encoder.encode(newItems)
+        let encodedData = try encoder.encode(items)
         
         setValue(encodedData, forKey: key.rawValue)
     }
--- KAppStoreSearchTests/KAppStoreSearchTests.swift
+++ KAppStoreSearchTests/KAppStoreSearchTests.swift
@@ -10,12 +10,21 @@ import XCTest
 final class KAppStoreSearchTests: XCTestCase {
     
     override func setUpWithError() throws {
+        let mockItem = ["TEST"]
+        let encodedMockItem = try! JSONEncoder().encode(mockItem)
+        UserDefaults.standard.setValue(encodedMockItem, forKey: UserDefaults.Key.test.rawValue) // Set UserDefaults Test Mock Data
     }
     
     override func tearDownWithError() throws {
     }
     
-    func test_UserDefaults_저장_가져오기() {
+    func test_UserDefaults_아이템_가져오기() {
+        let items: [String]? = UserDefaults.standard.fetch(key: .test)
+        
+        XCTAssertEqual(items, ["TEST"])
+    }
+    
+    func test_UserDefaults_아이템_저장() {
         let old: [String] = UserDefaults.standard.fetch(key: .test) ?? []
         
         let newItem = "NEW ITEM"
@@ -25,5 +34,35 @@ final class KAppStoreSearchTests: XCTestCase {
         let new: [String] = UserDefaults.standard.fetch(key: .test) ?? []
         
         XCTAssertEqual([newItem] + old, new)
+    }
+    
+    func test_UserDefaults_중복_아이템_저장() {
+        let newItem1 = "NEW ITEM"
+        
+        try? UserDefaults.standard.save(newItem1, key: .test)
+        
+        let newItem2 = "TEST"
+        
+        try? UserDefaults.standard.save(newItem2, key: .test)
+        
+        let new: [String] = UserDefaults.standard.fetch(key: .test) ?? []
+        
+        XCTAssertEqual(["TEST", "NEW ITEM"], new)
+    }
+    
+    func test_UserDefaults_모든_아이템_삭제() {
+        UserDefaults.standard.removeAll(key: .test)
+        
+        let items: [String]? = UserDefaults.standard.fetch(key: .test)
+        
+        XCTAssertNil(items)
+    }
+    
+    func test_UserDefaults_아이템_한개_삭제() {
+        try? UserDefaults.standard.remove("TEST", key: .test)
+        
+        let items: [String]? = UserDefaults.standard.fetch(key: .test)
+        
+        XCTAssertEqual(items, [])
     }
 }
